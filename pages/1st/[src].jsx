8b25a2b49de015ecac22d030546cab0952f3645a
cap_Yousef <93404648+capYousef@users.noreply.github.com>
2022-06-11 21:50:01 +0200

    Update [src].jsx

--- pages/1st/[src].jsx
+++ pages/1st/[src].jsx
@@ -6,30 +6,26 @@ export default function oldExamsPdf() {
   const { src } = router.query;
   const containerRef = useRef(null);
 
-     useEffect(() => {
-    if (typeof window !== "undefined") {
-      const container = containerRef.current;
-      let PSPDFKit;
-      (async function () {
-        PSPDFKit = await import("pspdfkit");
+  useEffect(() => {
+    const container = containerRef.current;
+    let PSPDFKit;
 
-        if (PSPDFKit) {
-          PSPDFKit.unload(container);
-        }
+    (async function () {
+      PSPDFKit = await import("pspdfkit");
+      
+      if (PSPDFKit) {
+        PSPDFKit.unload(container);
+      }
+      
+      await PSPDFKit.load({
+        container,
+      document: `/1st/oldExams/${src}.pdf`,
+        baseUrl: `${window.location.protocol}//${window.location.host}/`,
+      });
+    })();
 
-        await PSPDFKit.load({
-          container,
-          document: `/1st/oldExams/${src}.pdf`,
-          baseUrl: `${window.location.protocol}//${window.location.host}/`,
-        });
-      })();
-
-      return () => PSPDFKit && PSPDFKit.unload(container);
-    }
-    console.log(src);
+    return () => PSPDFKit && PSPDFKit.unload(container);
   }, []);
-
-
   return (
     <div>
       <button className="z-50 btn outline-2 hover:bg-mainColor1 bg-mainColor1 text-white fixed top-[80px] right-1">
