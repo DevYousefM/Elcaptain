434d3557357a796ec6759d7aa7471d1132d5caf4
cap_Yousef <93404648+capYousef@users.noreply.github.com>
2022-06-11 15:33:24 +0200

    Update [src].jsx

--- pages/1st/[src].jsx
+++ pages/1st/[src].jsx
@@ -30,7 +30,7 @@ export default function oldExamsPdf() {
 
   return (
                  <div>
-      <button className="z-50 btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor bg-mainColor text-white fixed top-[76px] right-0">
+      <button className="z-50 btn outline-2 hover:border-mainColor border-mainColor hover:bg-mainColor outline-mainColor hover:outline-mainColor bg-mainColor text-white fixed top-[76px] right-0">
         لتحميل الملف اضغط هنا
       </button>
       <div ref={containerRef} style={{ height: "100vh" }} />
