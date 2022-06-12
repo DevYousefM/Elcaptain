3383cadfd98c4da3a6847ce73fadf208efe54ffc
capYousef <elcaptain.yousef.official@gmail.com>
2022-06-13 00:20:42 +0200

    Upload Design

--- pages/1st/[src].jsx
+++ pages/1st/[src].jsx
@@ -4,46 +4,61 @@ import { useRouter } from "next/router";
 export default function oldExamsPdf() {
   const router = useRouter();
   const { src } = router.query;
+  console.log(src);
+
   const containerRef = useRef(null);
 
-  console.log(router.query.src);
   useEffect(() => {
-        if (typeof window !== "undefined") {
-
     const container = containerRef.current;
     let PSPDFKit;
-  console.log(router.query.src);
     (async function () {
-        console.log(router.query.src);
       PSPDFKit = await import("pspdfkit");
-      
+
       if (PSPDFKit) {
         PSPDFKit.unload(container);
       }
-        console.log(router.query.src);
+
       await PSPDFKit.load({
         container,
-      document: `/1st/oldExams/${src}.pdf`,
+        document: `/1st/oldExams/${src}.pdf`,
         baseUrl: `${window.location.protocol}//${window.location.host}/`,
       });
     })();
 
     return () => PSPDFKit && PSPDFKit.unload(container);
-        }
-    console.log(router.query.src);
   }, []);
+  console.log(src);
+
   return (
-    <div>
+    <div className=" pt-24">
       <button className="z-50 btn outline-2 hover:bg-mainColor1 bg-mainColor1 text-white fixed top-[80px] right-1">
         <a
           className="hover:text-white"
           href={`/1st/oldExams/${src}.pdf`}
           download
         >
-          <i className="fa-solid fa-download"></i>{" "}
+          <i className="fa-solid fa-download"></i>
         </a>
       </button>
       <div ref={containerRef} style={{ height: "100vh" }} />
     </div>
   );
+}
+export async function getStaticPaths() {
+  const btnsObjsArr = require("./1stApi.json");
+  const paramsPaths = btnsObjsArr
+    .map((obj) => obj.id)
+    .map((id) => ({ params: { src: id } }));
+
+  return {
+    paths: paramsPaths,
+    fallback: false,
+  };
+}
+export async function getStaticProps(ctx) {
+  return {
+    props: {
+      source: ctx.params.src,
+    },
+  };
 }
--- pages/1st/oldExams.jsx
+++ pages/1st/oldExams.jsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function OldExams() {
-  const router = useRouter();
+  const btnsObjsArr = require("./1stApi.json");
   return (
     <div className="w-full mt-4 pb-4 items-center flex flex-col border-b-2 ">
       <h1 className="text-center text-mainColor1 w-full text-4xl font-semibold relative">
@@ -40,58 +39,20 @@ export default function OldExams() {
       <div className="w-full flex flex-col items-center justify-center">
         <h3 className="mt-4 text-mainColor1">مواد الفصل الدراسي الثاني</h3>
         <div className="mt-3 w-3/5 gap-2 flex flex-wrap justify-center">
-          <button
-            onClick={() => {
-              router.push("/1st/oldSpaha");
-              console.log("done");
-            }}
-            className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor"
-          >
-            <Link href={"/1st/oldSpaha"}>
-              <a className="hover:text-white">اساسيات السباحة</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"/1st/oldMadrp"}>
-              <a className=" hover:text-white">اساسيات العاب المضرب</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            اساسيات المصارعة
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldTayra"}>
-              <a className="hover:text-white">اساسيات الكرة الطائرة</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            الجمباز الإيقاعي
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldPhysiology"}>
-              <a className="hover:text-white">فسيولوجيا الإنسان</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldElmHaraka"}>
-              <a className="hover:text-white">مبادئ علم الحركة</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldElmNafs"}>
-              <a className="hover:text-white">علم النفس الرياضي</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldTadrees"}>
-              <a className="hover:text-white">مبادئ التدريس</a>
-            </Link>
-          </button>
-          <button className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor">
-            <Link href={"oldEhsa"}>
-              <a className="hover:text-white">الاحصاء التطبيقي</a>
-            </Link>
-          </button>
+          {btnsObjsArr
+            .filter((y) => y.type == "one_St_Old_Exams")
+            .map((btn) => {
+              return (
+                <button
+                  key={Math.random()}
+                  className="btn outline-2 hover:border-mainColor hover:text-white border-mainColor outline-mainColor hover:outline-mainColor hover:bg-mainColor text-mainColor"
+                >
+                  <Link href={`/1st/${btn.id}`}>
+                    <a className="hover:text-white">{btn.content}</a>
+                  </Link>
+                </button>
+              );
+            })}
         </div>
       </div>
     </div>
