8484057eaa0369fa88e985d972d90d369da793b0
cap_Yousef <93404648+capYousef@users.noreply.github.com>
2022-06-13 00:49:13 +0200

    Update [src].jsx

--- pages/1st/[src].jsx
+++ pages/1st/[src].jsx
@@ -44,22 +44,3 @@ export default function oldExamsPdf() {
     </div>
   );
 }
-export async function getStaticPaths() {
-  const btnsObjsArr = require("./1stApi.json");
-  const paramsPaths = btnsObjsArr
-    .map((obj) => obj.id)
-    .map((id) => ({ params: { src: id } }));
-  console.log(btnsObjsArr);
-
-  return {
-    paths: paramsPaths,
-    fallback: false,
-  };
-}
-export async function getStaticProps(ctx) {
-  return {
-    props: {
-      source: ctx.params.src,
-    },
-  };
-}
